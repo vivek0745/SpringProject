0f0702fa36c155045426c99ef565f864f272b46b
vivek0745 <vivek.kumar0745@gmail.com>
2020-04-12 21:12:05 +0530

    added the peoject

---
+++ boot-filedownload/src/main/java/com/javainuse/SpringBootFileDownoad.java
@@ -0,0 +1,13 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileDownoad {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileDownoad.class, args);
+	}
+
+}
